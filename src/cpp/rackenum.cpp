43c353d7d924a272b13e1060d10cada8544c4832
Dirk Leas <dirkleas@gmail.com>
2018-04-19 23:48:59 -0400

    stubs

---
+++ src/cpp/rackenum.cpp
@@ -0,0 +1,10 @@
+
+/*
+  rackenum.cpp - plugin/module[s] introspection
+
+  introspect specified plugin shared library/dll and serialize plugin and module details as JSON
+
+  should provide: 1) CLI, and 2) swig-compatible C wrapper
+  CLI should support one or more .dylib/.dll references and return a JSON list
+*/
+
---
+++ src/go/rackcli.go
@@ -0,0 +1,13 @@
+
+/*
+  rackcli.go - vcv rack utility scaffolding
+*/
+
+package main 
+
+import "fmt"
+
+func main() {
+  fmt.Println("VCV Rack rulz! rackcli scaffolding on the way...")
+}
+
