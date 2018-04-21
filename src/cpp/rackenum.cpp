3c6a272dd20ff77c1cdd16262eb613732e38ba5d
Dirk Leas <dirkleas@gmail.com>
2018-04-20 22:39:58 -0400

    enum and iterate spike

--- src/cpp/rackenum.cpp
+++ src/cpp/rackenum.cpp
@@ -2,9 +2,48 @@
 /*
   rackenum.cpp - plugin/module[s] introspection
 
-  introspect specified plugin shared library/dll and serialize plugin and module details as JSON
+  introspect specified plugin shared library/dll and serialize plugin and
+  module details as JSON
 
   should provide: 1) CLI, and 2) swig-compatible C wrapper
   CLI should support one or more .dylib/.dll references and return a JSON list
 */
 
+#include <iostream>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <dlfcn.h>
+using namespace std;
+
+// minimal rack bits...
+
+bool systemIsFile(std::string path) {
+	struct stat statbuf;
+	if (stat(path.c_str(), &statbuf))
+		return false;
+	return S_ISREG(statbuf.st_mode);
+}
+
+int main() {
+  // just pick one, iterate on them all later
+  std::string libraryFilename = string(std::getenv("HOME")) +
+    "/Documents/Rack/plugins/cf/plugin.dylib";
+
+  if (! systemIsFile(libraryFilename)) {
+    std::cout << "no such plugin: " << libraryFilename << endl;
+    return 0;
+  }
+  
+  // dynamically load and introspect
+  void *handle = dlopen(libraryFilename.c_str(), RTLD_NOW);
+  if (!handle) {
+    std::cout << "Failed to load library " << libraryFilename << endl
+      << dlerror() << endl;
+  	return 0;
+  }
+
+  // introspect and serialize as JSON
+  std:cout << "[]" << endl;
+
+  return 1;
+}
--- src/go/rackcli.go
+++ src/go/rackcli.go
@@ -1,13 +1,24 @@
 
-/*
-  rackcli.go - vcv rack utility scaffolding
-*/
+// rackcli.go - vcv rack utility scaffolding
 
-package main 
+package main
 
-import "fmt"
+import ("fmt"; "strings"; "os"; "path/filepath")
 
 func main() {
-  fmt.Println("VCV Rack rulz! rackcli scaffolding on the way...")
-}
+  fmt.Println("VCV Rack rulz! rackcli scaffolding on the way, introspecting for now...")
 
+  if root := os.Getenv("RACK_HOME"); root != "" {
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+      if strings.HasSuffix(path, ".dylib") { // introspect each plugin/module(s)
+        fmt.Printf("%+v\n", path)
+      }
+      return nil
+    })
+    if err != nil {
+      fmt.Println("error walking rack installed rack plugins")
+    }
+  } else {
+    fmt.Println("missing RACK_HOME")
+  }
+}
